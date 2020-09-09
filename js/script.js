19c9a58d6535a80339dcb59041dc167c2d237318
Koka Mash <mr.rtte@gmail.com>
2020-09-09 22:43:41 +0700

    task 15

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 'use strict';
 
-let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "");
+let numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+while (numberOfFilms == 0 || numberOfFilms == null) {
+    numberOfFilms = +prompt("И всё же, сколько?", "");
+}
 
 let personalMovieDB = {
     count: numberOfFilms,
@@ -10,12 +13,25 @@ let personalMovieDB = {
     privat: false
 };
 
-let a = prompt("Один из просмотренных фильмов?", ""),
-    b = prompt("На сколько оцените его?", ""),
-    c = prompt("Один из просмотренных фильмов?", ""),
-    d = prompt("На сколько оцените его?", "");
-
-    personalMovieDB.movies[a] = b;
-    personalMovieDB.movies[c] = d;
+for (let i = 0; i < 2; i++) {
+    const a = prompt("Один из просмотренных фильмов?", ""),
+          b = +prompt("На сколько оцените его?", "");
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+    } else {
+        i--;
+    }
     
+}
+
+if (personalMovieDB.count < 10) {
+    alert('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    alert('Вы классический зритель');
+} else if (personalMovieDB.count >= 30) {
+    alert('Вы киноман');
+} else {
+    alert('Произошла ошибка');
+}
+
 console.log(personalMovieDB);
